72c1017d8c4337ad968254ad0ce1db8b0dd8ec44
Luiz Lopes <luizlopes12@outlook.com>
2022-03-16 13:28:54 -0300

    Funcionalidade de pesquisa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,28 @@
-$(document).ready(function(){
-    $("button").click(function(){
+$(document).ready(function () {
+  // console.log('funcionando')
 
-    })
-})
+  let form = $('#cepForm')
+  let cep = $('#cepInput')
+  form.submit(async function (e) {
+    e.preventDefault()
+    if(cep.val().length === 8){
+        console.log('foi')
+        $.ajax({
+            url: `http://viacep.com.br/ws/${cep.val()}/json/`,
+          }).done(function (data) {
+            const cepData = data;
+            console.log(cepData);
+          });
+    }else{
+        console.log('que louco')
+    }
+
+  });
+
+
+
+
+
+
+
+});
