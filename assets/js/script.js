a82b6e53f1855c7a3aca44fbe369134c152f430b
Luiz Lopes <luizlopes12@outlook.com>
2022-03-16 15:21:28 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,39 @@ $(document).ready(function () {
   let cep = $("#cepInput");
   let message = $("#message");
   let messageContent = "";
-  let alertMessage = $('#dialog')
-  alertMessage.hide();
+
   // Ao fazer submit no form, executa a verificação do CEP
-  form.submit(async function (e) {
+
+  form.submit(function (e) {
     e.preventDefault();
     if (cep.val().length === 8) {
-        $.ajax({
-          url: `http://viacep.com.br/ws/${cep.val()}/json/`,
-        }).done(function (data) {
-        // Pega os dados especificos no retorno e coloca eles na string messageContent
+      $.ajax({
+        url: `http://viacep.com.br/ws/${cep.val()}/json/`,
+      }).done(function (data) {
+
+        // Verificando se o CEP existe
+
+        if (data.erro) {
+          alert("CEP inválido");
+        } else {
+
+          // Captando os dados e formatando eles em uma string
+          // Alguns dos CEP não possuem rua nem bairro, também ocorre uma verificação
+
           const { bairro, logradouro: rua, localidade: cidade, uf, cep } = data;
           if (rua && bairro) {
             messageContent = `${rua} - ${bairro}, ${cidade} - ${uf}, ${cep}`;
           } else {
             messageContent = `${cidade} - ${uf}, ${cep}`;
           }
-        // Adiciona os dados formatados no campo de mensagem
+
+          // Adiciona os dados formatados no campo de mensagem
+
           message.text(messageContent);
-        });
+        }
+      });
     } else {
-        alert('CEP inválido');
+      alert("CEP inválido");
     }
   });
 });
