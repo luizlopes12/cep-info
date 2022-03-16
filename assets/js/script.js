f21544d283488eb2c659317fe609509d7874dd2f
Luiz Lopes <luizlopes12@outlook.com>
2022-03-16 15:10:13 -0300

    Finalização

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,29 @@
 $(document).ready(function () {
-  // console.log('funcionando')
-
-  let form = $('#cepForm')
-  let cep = $('#cepInput')
+  let form = $("#cepForm");
+  let cep = $("#cepInput");
+  let message = $("#message");
+  let messageContent = "";
+  let alertMessage = $('#dialog')
+  alertMessage.hide();
+  // Ao fazer submit no form, executa a verificação do CEP
   form.submit(async function (e) {
-    e.preventDefault()
-    if(cep.val().length === 8){
-        console.log('foi')
+    e.preventDefault();
+    if (cep.val().length === 8) {
         $.ajax({
-            url: `http://viacep.com.br/ws/${cep.val()}/json/`,
-          }).done(function (data) {
-            const cepData = data;
-            console.log(cepData);
-          });
-    }else{
-        console.log('que louco')
+          url: `http://viacep.com.br/ws/${cep.val()}/json/`,
+        }).done(function (data) {
+        // Pega os dados especificos no retorno e coloca eles na string messageContent
+          const { bairro, logradouro: rua, localidade: cidade, uf, cep } = data;
+          if (rua && bairro) {
+            messageContent = `${rua} - ${bairro}, ${cidade} - ${uf}, ${cep}`;
+          } else {
+            messageContent = `${cidade} - ${uf}, ${cep}`;
+          }
+        // Adiciona os dados formatados no campo de mensagem
+          message.text(messageContent);
+        });
+    } else {
+        alert('CEP inválido');
     }
-
   });
-
-
-
-
-
-
-
 });
